7472e78ea59940ce6145a7ba31a358f87a0c83ff
Ivy Reed <idcfu788@gmail.com>
2023-07-13 17:29:57 -0400

    cleaned up code and added notes

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const app = express();
 const PORT = 4321;
 
 app.use(express.static('public'));
+
+// I found out I left these lines of code out after I got the body parsing working, so they are here for referencing this in the future.
 // app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
+
+
 app.use('/api', api)
 app.use('/', html)
 
